1ac942c64c5b76ab70d4820cc03f785165af2be4
Georg Ofenbeck <georg@ofenbeck.com>
2025-02-02 16:42:08 +0100

    copilot prompt / octo - merge with neovim

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,24 @@
+local prompts = {
+	-- Code related prompts
+	Explain = "Please explain how the following code works.",
+	Review = "Please review the following code and provide suggestions for improvement.",
+	Tests = "Please explain how the selected code works, then generate unit tests for it.",
+	Refactor = "Please refactor the following code to improve its clarity and readability.",
+	FixCode = "Please fix the following code to make it work as intended.",
+	FixError = "Please explain the error in the following text and provide a solution.",
+	BetterNamings = "Please provide better names for the following variables and functions.",
+	Documentation = "Please provide documentation for the following code.",
+	PythonMe = "Please explain the following Python code to me. Remember - I am an expert scala developer - so comparing to Scala is very helpful for me",
+	-- Text related prompts
+	Summarize = "Please summarize the following text.",
+	Spelling = "Please correct any grammar and spelling errors in the following text.",
+	Wording = "Please improve the grammar and wording of the following text.",
+	Concise = "Please rewrite the following text to make it more concise.",
+}
+
+return {
+	"CopilotC-Nvim/CopilotChat.nvim",
+	opts = {
+		prompts = prompts,
+	},
+}
